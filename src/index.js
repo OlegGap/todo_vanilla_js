026e0295a71ee7432d2a45d907d0c1ba8c3855c4
oleg <hnapovskiy20@gmail.com>
2019-10-30 16:19:00 +0200

    add 'delete card'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import './styles/style.scss';
-import cards from './db/cards.json';
+import { allCards } from './db/cards.json';
 
 const filters = {
-  value: 'all',
+  value: '',
   status: 'all',
   priority: 'all',
 };
+
+const applyFilter = (newFilters, cards) => {
+  let resultCurds = cards.slice(0);
+
+  if (newFilters.value !== '') {
+    const reg = new RegExp(newFilters.value);
+    resultCurds = cards.filter(card => card.title.match(reg) !== null);
+  }
+  if (newFilters.status !== 'all') {
+    resultCurds = resultCurds.filter(card => card.status === newFilters.status);
+  }
+  if (newFilters.priority !== 'all') {
+    resultCurds = resultCurds.filter(
+      card => card.priority === newFilters.priority,
+    );
+  }
+  loadCards(resultCurds);
+};
+
+const hundleCardSearch = event => {
+  event.preventDefault();
+  filters.value = event.target.querySelector('input[name="search"]').value;
+  applyFilter(filters, allCards);
+};
+const serchForm = document.querySelector('.navigation__search');
+serchForm.addEventListener('submit', hundleCardSearch);
 
 const filterStatusContentNode = document.querySelector(
   '.navigation__search_filter-status-content',
@@ -16,6 +42,10 @@ const filterStatusCurrentNode = document.querySelector(
 const hundleChangeStatus = ({ target }) => {
   const currentSelector = filterStatusCurrentNode.childNodes[0].nodeValue.trim();
   filterStatusCurrentNode.childNodes[0].nodeValue = target.innerText;
+
+  filters.status = target.innerText;
+  applyFilter(filters, allCards);
+
   target.innerText = currentSelector;
 };
 filterStatusContentNode.addEventListener('click', hundleChangeStatus);
@@ -29,19 +59,69 @@ const filterPriorityCurrentNode = document.querySelector(
 const hundleChangePriority = ({ target }) => {
   const currentSelector = filterPriorityCurrentNode.childNodes[0].nodeValue.trim();
   filterPriorityCurrentNode.childNodes[0].nodeValue = target.innerText;
+
+  filters.priority = target.innerText;
+  applyFilter(filters, allCards);
+
   target.innerText = currentSelector;
 };
 filterPriorityContentNode.addEventListener('click', hundleChangePriority);
+loadCards(allCards);
 
-const cardsList = document.querySelector('.cards-list');
-let card;
-cards.map((cardData, idx) => {
-  card = document.createElement('div');
-  card.className = 'card-wrapper';
+function loadCards(cards) {
+  const cardsList = document.querySelector('.cards-list');
+  cardsList.innerHTML = '';
+  let card;
+  cards.map((cardData, idx) => {
+    card = document.createElement('div');
+    card.className = 'card-wrapper';
 
-  card.innerHTML = `<div data-id="${idx}" class="card"> <div class="card-title">${cardData.title}</div></div>`;
-  card.lastChild.innerHTML += `<div class="card-content">${cardData.text}</div>`;
+    card.innerHTML = `<div data-id="${cardData.id}" class="card"> <div class="card-title">${cardData.title}</div></div>`;
+    if (cardData.status === 'done') {
+      card.lastChild.innerHTML += `<div class="card-mark">&#10003;</div>`;
+      card.classList.add('card-done');
+    }
+    card.lastChild.innerHTML += `<div class="card-content">${cardData.text}</div>`;
 
-  card.lastChild.innerHTML += `<div class="card__footer"><div class="card__footer-priority">${cardData.priority}</div><div class="card__footer-dropdown__navigation"><button class="card__footer-dropdown__navigation-button"> ...</button><div class="card__footer-dropdown__navigation-content"><button>done</button><button>edit</button><button>delete</button></div> </div> </div>`;
-  cardsList.appendChild(card);
-});
+    card.lastChild.innerHTML += `<div class="card__footer"><div class="card__footer-priority">${
+      cardData.priority
+    }</div><div class="card__footer-dropdown__navigation"><button class="card__footer-dropdown__navigation-button"> ...</button><div class="card__footer-dropdown__navigation-content"><button data-card='status'>${
+      cardData.status === 'done' ? 'open' : 'done'
+    }</button data-card='edit'><button>edit</button><button data-card='delete'>delete</button></div> </div> </div>`;
+    cardsList.appendChild(card);
+
+    const cardCurrentButtonStatusNode = document.querySelectorAll(
+      'button[data-card="status"]',
+    );
+    cardCurrentButtonStatusNode[idx].addEventListener('click', () =>
+      hundleChangeCardStatus(cardData.id, cardData.status),
+    );
+
+    const cardCurrentButtonDeleteNode = document.querySelectorAll(
+      'button[data-card="delete"]',
+    );
+    cardCurrentButtonDeleteNode[idx].addEventListener('click', () =>
+      hundleDeleteCard(cardData.id),
+    );
+  });
+}
+
+function hundleChangeCardStatus(id, status) {
+  allCards.map(card => {
+    if (card.id === id) {
+      card.status = status === 'done' ? 'open' : 'done';
+    }
+    return card;
+  });
+  loadCards(allCards);
+}
+
+function hundleDeleteCard(id) {
+  allCards.map((card, idx) => {
+    if (card.id === id) {
+      allCards.splice(idx, 1);
+    }
+    return card;
+  });
+  loadCards(allCards.filter(card => card.id !== id));
+}
